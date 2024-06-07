bb62e4647135d68145579a78ec6043700e263f61
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-07 11:28:24 -0300

    update

--- src/pages/update-students.tsx
+++ src/pages/update-students.tsx
@@ -6,7 +6,7 @@ export function UpdateStudents() {
           Atualizar alunos
         </h2>
 
-        <div className="group relative mx-auto my-8 h-[46rem] max-h-screen  max-w-4xl overflow-auto rounded bg-pmpa-blue-700">
+        <div className="group relative mx-auto my-8 h-80 max-h-screen  max-w-4xl overflow-auto rounded bg-pmpa-blue-700">
           <form className="mx-auto space-y-4 p-6">
             <div className="space-y-1">
               <label htmlFor="name" className="text-sm text-gray-200">

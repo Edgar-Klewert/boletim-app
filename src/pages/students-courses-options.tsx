1d795e972554a9dd7324e0b7f3fe8d712636fa43
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-20 12:45:43 -0300

    fetch: creating course options for students

---
+++ src/pages/students-courses-options.tsx
@@ -0,0 +1,61 @@
+import PlusIcon from '@/assets/add-plus-icon.png'
+import UpdateIcon from '@/assets/update-icon.png'
+import RemoveIcon from '@/assets/remove-icon.png'
+import { Link } from 'react-router-dom'
+
+export function StudentsCoursesOptions() {
+  return (
+    <section className="mx-auto w-full max-w-[90rem] px-2 py-10 md:py-6">
+      <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+        Opções curso
+      </h2>
+
+      <div className="mx-auto my-6 grid w-full max-w-7xl grid-cols-1 justify-items-center gap-4 py-2 md:grid-cols-2 md:justify-items-start lg:grid-cols-3">
+        <Link
+          to={'#'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={PlusIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">KTS</span>
+        </Link>
+        <Link
+          to={'#'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={UpdateIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">Informações Aluno</span>
+        </Link>        
+        <Link
+          to={'#'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={RemoveIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">Boletim</span>
+        </Link>
+        <Link
+          to={'#'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={RemoveIcon}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">Confirmação de notas</span>
+        </Link>
+      </div>
+    </section>
+  )
+}

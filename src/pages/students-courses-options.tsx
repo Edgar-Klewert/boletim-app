fc3db2e49590c98dfc8200fe5c25674929572df0
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-24 13:22:13 -0300

    feat: student courses options

--- src/pages/students-courses-options.tsx
+++ src/pages/students-courses-options.tsx
@@ -32,7 +32,7 @@ export function StudentsCoursesOptions() {
             alt=""
           />
           <span className="font-medium text-white">Informações Aluno</span>
-        </Link>        
+        </Link>
         <Link
           to={'#'}
           className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"

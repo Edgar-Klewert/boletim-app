12d0c4dd3f32fbb57a4ff86a45b8215a5475aa89
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-25 17:52:09 -0300

    refactor: change students layout

--- src/pages/search-students-to-update.tsx
+++ src/pages/search-students-to-update.tsx
@@ -1,6 +1,6 @@
 export function SearchStudentsToUpdate() {
   return (
-    <div className="w-full py-6">
+    <div className="w-full py-6"> 
       <section className="mx-auto w-full max-w-[90rem]">
         <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
           Buscar alunos

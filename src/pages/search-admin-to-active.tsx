51d4e13baedb3da6de8692ffdc305b0f958853b8
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-26 17:29:59 -0300

    feat: active search

---
+++ src/pages/search-admin-to-active.tsx
@@ -0,0 +1,25 @@
+export function SearchAdminToActive(){
+    return(
+        <div className="w-full py-6">
+            <section className="mx-auto w-full max-w-[90rem]">
+                <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+                    Ativar Administrador
+                </h2>
+
+                <form className="my-8 flex flex-col items-center justify-center gap-6">
+                    <h3 className="text-2xl font-bold">
+                        Pesquisar pelo administrador que deseja ativar
+                    </h3>
+                    <div className="flex w-full max-w-3xl items-center justify-center gap-4">
+                        <input 
+                          type="text"
+                          className="w-full rounded-sm p-3"
+                          placeholder="John Doe..." 
+                        />
+                        <button className="hidden">Buscar</button>
+                    </div>
+                </form>
+            </section>
+        </div>
+    )
+}

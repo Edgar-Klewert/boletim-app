eb8ca93d3e9976ae65d2678845ac436c8152abd7
Edgar-Klewert <edgarklewert66@gmail.com>
2024-06-27 17:10:05 -0300

    feat: create SearchNotesToRemove

---
+++ src/pages/search-notes-to-remove.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+export function SearchNotesToRemove() {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [disciplinas] = useState([
+    
+    'Armamento Policial',
+    'Direito Penal',
+    'Direito Processual Penal',
+    'Legislação Penal Especial',
+    'Criminologia',
+    'Polícia Judiciária',
+    'Investigação Criminal',
+    'Técnicas de Interrogatório',
+    'Ciência Forense',
+    'Balística Forense',
+    'Documentoscopia',
+    'Datiloscopia',
+    'Fotografia Forense',
+    'Medicina Legal',
+    'Odontologia Legal',
+    'Psicologia Forense',
+    'Sociologia Jurídica',
+    'Direitos Humanos',
+    'Ética Profissional',
+    'Administração Policial',
+    'Gestão de Pessoas',
+    'Planejamento Estratégico',
+    'Orçamento Público',
+    'Logística Policial',
+    'Comunicação Policial',
+    'Relações Públicas',
+    'Atendimento ao Público',
+    'Segurança Pública',
+    'Inteligência Policial',
+    'Controle de Multidões',
+    'Operações Policiais Especiais',
+    'Táticas Policiais',
+    'Defesa Pessoal',
+    'Atividade Física',
+    'Primeiros Socorros',
+  ]);
+
+  const filteredDisciplinas = searchTerm
+    ? disciplinas.filter((disciplina) =>
+        disciplina.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : disciplinas;
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Disciplinas</h1>
+      <div className="flex items-center mb-4">
+        <input
+          type="text"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Pesquise pela disciplina"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {filteredDisciplinas.map((disciplina, index) => (
+          <div
+            key={index}
+            className="bg-pmpa-blue-500 hover:bg-pmpa-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline"
+          >
+            <a href={`/disciplinas/${disciplina.toLowerCase()}`}>
+              {disciplina}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

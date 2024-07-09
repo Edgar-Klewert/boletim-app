59266fb06d4e0fca883bd3d233671d091024c9ba
Edgar-Klewert <edgarklewert66@gmail.com>
2024-07-09 16:45:11 -0300

    refactor: change the delete course layout

--- src/pages/delete-courses.tsx
+++ src/pages/delete-courses.tsx
@@ -1,26 +1,42 @@
-export function DeleteCourses() {
-  return (
-    <main className="w-full px-2 py-6">
-      <section className="mx-auto w-full max-w-7xl">
-        <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
-          Escolher curso
-        </h2>
+import CFO from '@/assets/cfo-img-curso.jpg'
 
-        <div className="mx-auto my-8 grid w-full max-w-6xl grid-cols-3">
-          <div className="flex flex-col items-center space-y-2 px-2">
-            <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-            <span className="font-medium">CFP - 2022</span>
-          </div>
-          <div className="flex flex-col items-center space-y-2 px-2">
-            <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-            <span className="font-medium">CAS - 2023</span>
-          </div>
-          <div className="flex flex-col items-center space-y-2 px-2">
-            <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-            <span className="font-medium">CFO - 2022</span>
-          </div>
-        </div>
-      </section>
-    </main>
+const courses = [
+    { id: 1, title: 'CAS - 2023' },
+    { id: 2, title: 'CAS TURMA I - 2024' },
+    { id: 3, title: 'CAS TURMA II - 2023' },
+];
+
+export function DeleteCourses(){
+  return(
+      <div className="w-full py-6">
+          <section className="mx-auto w-full max-w-[90rem]">
+              <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+                  Deletar cursos
+              </h2>
+
+              <div className="flex flex-wrap justify-center">
+                  {courses.map((course) =>(
+                      <div key={course.id} className="bg-white shadow-md m-10 w-80 py-1">
+                          <div className="flex flex-col items-center">
+                              <img src={CFO} alt="Polícia Militar" className='mb-4' />
+                              <h3 className='text-xl mb-2'>{course.title}</h3>
+                              <button className='bg-pmpa-blue-500 text-white py-2 px-4 rounded'>Deletar</button>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+              <div className="flex flex-wrap justify-center">
+                  {courses.map((course) =>(
+                      <div key={course.id} className="bg-white shadow-md m-10 w-80 py-1">
+                          <div className="flex flex-col items-center">
+                              <img src={CFO} alt="Polícia Militar" className='mb-4' />
+                              <h3 className='text-xl mb-2'>{course.title}</h3>
+                              <button className='bg-pmpa-blue-500 text-white py-2 px-4 rounded'>Deletar</button>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+          </section>
+      </div>
   )
 }

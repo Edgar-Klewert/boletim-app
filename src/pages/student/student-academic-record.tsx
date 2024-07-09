35398eb4486ce6c4391325fc9affb7aeb2bed84f
Edgar-Klewert <edgarklewert66@gmail.com>
2024-07-09 16:48:05 -0300

    feat: creation of the course transcript

---
+++ src/pages/student/student-academic-record.tsx
@@ -0,0 +1,26 @@
+import Course from '@/assets/my-course-icon.png'
+import { Link } from 'react-router-dom'
+
+export function StudentAcademicRecord() {
+  return (
+    <section className="mx-auto w-full max-w-[90rem] px-2 py-10 md:py-6">
+      <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+        Histórico Escolar
+      </h2>
+
+      <div className="mx-auto my-6 grid w-full max-w-7xl grid-cols-1 justify-items-center gap-4 py-2 md:grid-cols-2 md:justify-items-start lg:grid-cols-3">
+        <Link
+          to={'/student/academic-record/courses'}
+          className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
+        >
+          <img
+            src={Course}
+            className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2"
+            alt=""
+          />
+          <span className="font-medium text-white">Escolher Curso</span>
+        </Link>
+      </div>
+    </section>
+  )
+}

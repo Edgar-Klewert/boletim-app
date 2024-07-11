c17095cb344d49e2db3795ae4f1e1e9817a69e04
Edgar-Klewert <edgarklewert66@gmail.com>
2024-07-11 16:31:03 -0300

    refactor: change students layout

--- src/pages/search-students-to-update.tsx
+++ src/pages/search-students-to-update.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+
 export function SearchStudentsToUpdate() {
+
+  const [selectedFilter, setSelectedFilter] = useState('Todos');
+
   return (
     <div className="w-full py-6"> 
       <section className="mx-auto w-full max-w-[90rem]">
@@ -17,6 +22,19 @@ export function SearchStudentsToUpdate() {
               placeholder="John Doe..."
             />
             <button className="hidden">Buscar</button>
+          </div>
+          <div className="items-center space-x-0">
+            <h2 className="flex w-full justify-center py-3 font-semibold">SELECIONE O POLO</h2>
+            <select 
+            id="filter_search_students_to_update"
+            value={selectedFilter}
+            onChange={(e)=> setSelectedFilter(e.target.value)}
+            className='flex shadow rounded-lg border border-gray-500 w-full py-1 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'>
+              <option value="todos">TODOS OS POLOS</option>
+              <option value="cfap">CFAP</option>
+              <option value="castanhal">CASTANHAL</option>
+              <option value="santarem">SANTARÉM</option>
+            </select>
           </div>
         </form>
       </section>

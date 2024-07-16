feed97ceb7c2e3e16a48bf61f4298d76992ba346
Edgar-Klewert <edgarklewert66@gmail.com>
2024-07-16 16:56:40 -0300

    refactor: adjust route

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
 export function Header({ isLogged }: HeaderProps) {
   return (
     <header className="flex flex-col items-center justify-center border-b-[1px] border-red-600 bg-white px-2 py-2 md:h-16 md:flex-row md:items-center md:justify-between md:py-0">
-      <Link to={'/'} className="flex items-center md:gap-4">
+      <Link to={'/dev/home'} className="flex items-center md:gap-4">
         <img src={LogoBoletim} alt="Logo" className="flex h-10 w-auto" />
         <h1 className="text-lg font-bold uppercase">
           Boletim Acadêmico - PMPA

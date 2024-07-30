9eccc7ed2ae5b1068d7c958b69d63b9c6e158808
Edgar-Klewert <edgarklewert66@gmail.com>
2024-07-30 16:30:48 -0300

    feat: design improvements and adjustments

--- src/pages/general-classification.tsx
+++ src/pages/general-classification.tsx
@@ -24,23 +24,78 @@ export function GeneralClassification() {
   return (
     <div className="w-full py-6">
       <section className="mx-auto w-full max-w-[90rem]">
-        <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
+        <h2 className='w-full border-b-2 mb-4 border-b-black text-xl font-semibold'>
           Classificação
         </h2>
-
-        <div className="flex justify-between items-center mb-2  ">
-          <div className="flex items-center">
-          </div>
-          <div className="flex-1 py-10 items-center text-center">
-            <span className="bg-pmpa-blue-700 text-white font-bold py-4 px-28 rounded">Gráfico de notas</span>
-          </div>
-        </div>
-        <div className="bg-pmpa-blue-500 rounded-lg p-6">
+        <div className="bg-pmpa-blue-500 rounded-lg p-4 mb-4 flex justify-center items-center">
+          <div className="w-1/3 text-center text-white font-bold">Notas Muito Boas</div>
           <div className="w-1/4">
             <Pie data={data} options={options} />
           </div>
         </div>
-        <h3 className="text-black text-center font-bold mt-4">Classificação Geral: CAS - 2023</h3>
+
+        <div className="text-center font-bold mb-6">
+          <span className="text-black">Classificação Geral: CAS - 2023</span>
+        </div>
+
+        <div className="bg-white border border-gray-200 rounded-lg shadow-md">
+          <table className="min-w-full">
+            <thead>
+              <tr className="bg-pmpa-blue-500 border-b">
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">CLASS</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">RG</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">NOME COMPLETO</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">MÉDIA FINAL</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">CONCEITO</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">DATA DE NASCIMENTO</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">POLO</th>
+                <th className="py-2 px-4 text-left text-sm font-semibold text-white">STATUS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="py-2 px-4 text-sm text-slate-700">1º</td>
+                <td className="py-2 px-4 text-sm text-slate-700">23826</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Igor Bomba</td>
+                <td className="py-2 px-4 text-sm text-slate-700">9.784</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Muito Bom</td>
+                <td className="py-2 px-4 text-sm text-slate-700">01/01/1975</td>
+                <td className="py-2 px-4 text-sm text-slate-700">SANTARÉM</td>
+                <td className="py-2 px-4 text-sm text-slate-700">APROVADO</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 text-sm text-slate-700">2º</td>
+                <td className="py-2 px-4 text-sm text-slate-700">23751</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Alan Gay</td>
+                <td className="py-2 px-4 text-sm text-slate-700">9.724</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Muito Bom</td>
+                <td className="py-2 px-4 text-sm text-slate-700">01/01/1924</td>
+                <td className="py-2 px-4 text-sm text-slate-700">SANTARÉM</td>
+                <td className="py-2 px-4 text-sm text-slate-700">APROVADO</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 text-sm text-slate-700">3º</td>
+                <td className="py-2 px-4 text-sm text-slate-700">23751</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Felipe Fumas</td>
+                <td className="py-2 px-4 text-sm text-slate-700">9.700</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Muito Bom</td>
+                <td className="py-2 px-4 text-sm text-slate-700">01/01/2000</td>
+                <td className="py-2 px-4 text-sm text-slate-700">SANTARÉM</td>
+                <td className="py-2 px-4 text-sm text-slate-700">APROVADO</td>
+              </tr>
+              <tr>
+                <td className="py-2 px-4 text-sm text-slate-700">4º</td>
+                <td className="py-2 px-4 text-sm text-slate-700">23751</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Paulo Coleta </td>
+                <td className="py-2 px-4 text-sm text-slate-700">9.724</td>
+                <td className="py-2 px-4 text-sm text-slate-700">Muito Bom</td>
+                <td className="py-2 px-4 text-sm text-slate-700">01/01/1924</td>
+                <td className="py-2 px-4 text-sm text-slate-700">SANTARÉM</td>
+                <td className="py-2 px-4 text-sm text-slate-700">APROVADO</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </div>
   );

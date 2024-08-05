f22af3c98109aca6848ed93abc6ed0c6fb9bc0d2
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-05 17:36:09 -0300

    feat: adding a new route

--- src/pages/classification.tsx
+++ src/pages/classification.tsx
@@ -25,7 +25,7 @@ export function Classification() {
           <span className="font-medium text-white">Classificação geral</span>
         </Link>
         <Link
-          to={'#'}
+          to={'/classification/classification-poles'}
           className="relative flex h-52 w-full max-w-80 justify-end rounded bg-pmpa-blue-700 px-6 py-4"
         >
           <img

b1dd9d597ec28e0614dcda2f8cb9b99d9a92992f
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-06 12:49:38 -0300

    refactor: adjust route

--- src/pages/classification.tsx
+++ src/pages/classification.tsx
@@ -9,7 +9,7 @@ export function Classification() {
   return (
     <section className="mx-auto w-full max-w-[90rem] px-2 py-10 md:py-6">
       <h2 className="w-full border-b-2 border-b-black text-xl font-semibold">
-        Gerenciamento de dados
+        Classificação
       </h2>
 
       <div className="mx-auto my-6 grid w-full max-w-7xl grid-cols-1 justify-items-center gap-4 py-2 md:grid-cols-2 md:justify-items-start lg:grid-cols-3">

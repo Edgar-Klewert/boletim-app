0ec00ab838fc85919d7be128afad209ca872e354
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-12 17:44:23 -0300

    feat: design improvements and adjustments

--- src/components/templates/pole-classification-viewer.tsx
+++ src/components/templates/pole-classification-viewer.tsx
@@ -61,44 +61,44 @@ const styles = StyleSheet.create({
   },
 }); 
 
-const PoleClassificationViewer: React.FC<PoleClassificationViewerProps> = ({ data }) => (
-  <Document>
-    <Page size={{ width: 800, height: 1200 }} style={styles.page}>
-      {Object.entries(data).map(([polo, rows]) => (
-        <View style={styles.section} key={polo}>
-          <Text style={styles.title}>Classificação por Polo: {polo}</Text>
-          <View style={styles.table}>
-            <View style={styles.tableRow}>
-              <Text style={styles.tableCell}>CLASS</Text>
-              <Text style={styles.tableCell}>Q.AV</Text>
-              <Text style={styles.tableCell}>Q.C</Text>
-              <Text style={styles.tableCell}>RG</Text>
-              <Text style={styles.tableCell}>NOME COMPLETO</Text>
-              <Text style={styles.tableCell}>MÉDIA FINAL</Text>
-              <Text style={styles.tableCell}>CONCEITO</Text>
-              <Text style={styles.tableCell}>DATA DE NASCIMENTO</Text>
-              <Text style={styles.tableCell}>POLO</Text>
-              <Text style={styles.lastTableCell}>STATUS</Text>
-            </View>
-            {rows.map((row, index) => (
-              <View style={styles.tableRow} key={index}>
-                <Text style={styles.tableCell}>{row.class}</Text>
-                <Text style={styles.tableCell}>{row.qav}</Text>
-                <Text style={styles.tableCell}>{row.qc}</Text>
-                <Text style={styles.tableCell}>{row.rg}</Text>
-                <Text style={styles.tableCell}>{row.name}</Text>
-                <Text style={styles.tableCell}>{row.average}</Text>
-                <Text style={styles.tableCell}>{row.concept}</Text>
-                <Text style={styles.tableCell}>{row.dob}</Text>
-                <Text style={styles.tableCell}>{row.polo}</Text>
-                <Text style={styles.lastTableCell}>{row.status}</Text>
+export function PoleClassificationViewer({ data }: PoleClassificationViewerProps) {
+  return (
+    <Document>
+      <Page size={{ width: 800, height: 1200 }} style={styles.page}>
+        {Object.entries(data).map(([polo, rows]) => (
+          <View style={styles.section} key={polo}>
+            <Text style={styles.title}>Classificação por Polo: {polo}</Text>
+            <View style={styles.table}>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>CLASS</Text>
+                <Text style={styles.tableCell}>Q.AV</Text>
+                <Text style={styles.tableCell}>Q.C</Text>
+                <Text style={styles.tableCell}>RG</Text>
+                <Text style={styles.tableCell}>NOME COMPLETO</Text>
+                <Text style={styles.tableCell}>MÉDIA FINAL</Text>
+                <Text style={styles.tableCell}>CONCEITO</Text>
+                <Text style={styles.tableCell}>DATA DE NASCIMENTO</Text>
+                <Text style={styles.tableCell}>POLO</Text>
+                <Text style={styles.lastTableCell}>STATUS</Text>
               </View>
-            ))}
+              {rows.map((row, index) => (
+                <View style={styles.tableRow} key={index}>
+                  <Text style={styles.tableCell}>{row.class}</Text>
+                  <Text style={styles.tableCell}>{row.qav}</Text>
+                  <Text style={styles.tableCell}>{row.qc}</Text>
+                  <Text style={styles.tableCell}>{row.rg}</Text>
+                  <Text style={styles.tableCell}>{row.name}</Text>
+                  <Text style={styles.tableCell}>{row.average}</Text>
+                  <Text style={styles.tableCell}>{row.concept}</Text>
+                  <Text style={styles.tableCell}>{row.dob}</Text>
+                  <Text style={styles.tableCell}>{row.polo}</Text>
+                  <Text style={styles.lastTableCell}>{row.status}</Text>
+                </View>
+              ))}
+            </View>
           </View>
-        </View>
-      ))}
-   </Page>
-  </Document>
-);
-
-export default PoleClassificationViewer;
+        ))}
+     </Page>
+    </Document>
+  );
+}

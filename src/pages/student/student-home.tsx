1fded2f221f3c46482bcb0fcc62f9046926f8fe2
Edgar-Klewert <edgarklewert66@gmail.com>
2024-08-12 17:43:27 -0300

    feat: design improvements and adjustments

--- src/pages/student/student-home.tsx
+++ src/pages/student/student-home.tsx
@@ -1,29 +1,35 @@
+import { Link } from 'react-router-dom'
+import CFOImage from '@/assets/cfo-img-curso.jpg'
+import CASImage from '@/assets/cas-img-course.png'
+import CGSImage from '@/assets/cgs-img-course.png'
 
-export function StudentHome() {
-    return (
-      <div className="w-full py-6">
-          <section className="mx-auto w-full max-w-[90rem]">
-              <h2 className="w-full border-b-2 border-b-black text-xl font-semibold ">
-                  Meus cursos
-              </h2>
-  
-              <div className="mx-auto my-8 grid w-full max-w-6xl grid-cols-3">
-            <div className="flex flex-col items-center space-y-2 px-2">
-              <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-              <span className="font-medium">CFP - 2022</span>
-            </div>
-            <div className="flex flex-col items-center space-y-2 px-2">
-              <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-              <span className="font-medium">CAS - 2023</span>
-            </div>
-            <div className="flex flex-col items-center space-y-2 px-2">
-              <div className="h-60 w-full max-w-72 bg-gray-400"></div>
-              <span className="font-medium">CFO - 2022</span>
-            </div>
-  
-           </div>
-          </section>
-      </div>
-    );
-  }
-  
+const courses = [
+    { id: 1, title: 'CFO - 2023', imageURL: CFOImage },
+    { id: 2, title: 'CAS TURMA I - 2024', imageURL: CASImage},
+    { id: 3, title: 'CGS TURMA II - 2023', imageURL: CGSImage},
+];
+
+export function StudentHome(){
+    return(
+        <div className='w-full py-6'>
+            <section className='mx-auto w-full max-w-[90rem]'>
+                <h2 className='w-full border-b-2 border-b-black text-xl font-semibold'>
+                BEM-VINDO(A), "Aluno"
+                </h2>
+
+                <div className='flex flex-wrap justify-center'>
+                    {courses.map((course) =>(
+                        <div key={course.id} className='bg-white shadow-md m-10 w-80 py-1'>
+                            <Link to={`/students/page`} className='block'>
+                                <div className='flex flex-col items-center'>
+                                    <img src={course.imageURL} alt="Imagem do curso" className='mb-4' />
+                                    <h3 className='text-xl mb-2'>{course.title}</h3>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </div>
+    )
+}
